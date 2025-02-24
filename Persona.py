52a03c7b2de06aed5b3f0a875e2ffb0e1afb01ab
pc# <pc@gmail.com>
2025-02-24 11:25:21 -0500

    Archivo person v1

--- Persona.py
+++ Persona.py
@@ -1,2 +1,26 @@
-Persona:
-   
+class Persona:
+    def __init__(self, nombre, edad, direccion):
+        self._nombre = nombre
+        self._edad = edad
+        self._direccion = direccion
+
+    def get_nombre(self):
+        return self._nombre
+
+    def set_nombre(self, nombre):
+        self._nombre = nombre
+
+    def get_edad(self):
+        return self._edad
+
+    def set_edad(self, edad):
+        self._edad = edad
+
+    def get_direccion(self):
+        return self._direccion
+
+    def set_direccion(self, direccion):
+        self._direccion = direccion
+
+    def __str__(self):
+        return f"El nombre del estudiante es {self._nombre}, su edad es: {self._edad}, y vive en {self._direccion})"
